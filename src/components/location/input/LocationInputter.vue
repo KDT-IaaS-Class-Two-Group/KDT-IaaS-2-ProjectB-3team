73cafb82bfbeb6083d0880462d418bac7fda2b3e
최유진 <yjyj0774@gmail.com>
2024-10-01 16:26:10 +0900

    :art: 최유진 : location input 디자인 수정

--- src/components/location/input/LocationInputter.vue
+++ src/components/location/input/LocationInputter.vue
@@ -1,6 +1,10 @@
 <template>
   <div class="flex justify-center">
-    <input type="input" />
+    <input
+      type="input"
+      placeholder="장소, 주소 검색"
+      class="w-[500px] h-10 rounded-2xl border-2 px-4 my-6 hover:border-main-color focus:outline-none focus:border-main-color"
+    />
   </div>
 </template>
 
