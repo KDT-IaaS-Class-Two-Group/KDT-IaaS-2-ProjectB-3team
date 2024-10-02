c56b2764464ae24e6719dd8439780cdf54796fae
최유진 <yjyj0774@gmail.com>
2024-10-02 12:39:34 +0900

    :bulb: 최유진 : todo 주석 이동

--- src/modules/map/loadMap.ts
+++ src/modules/map/loadMap.ts
@@ -3,6 +3,7 @@ import { initMap } from "./initMap";
 export const loadMap = () => {
   const mapScript = document.createElement("script");
 
+  // todo : API url 환경변수 설정
   mapScript.src = `//dapi.kakao.com/v2/maps/sdk.js?appkey=${process.env.VUE_APP_API_KEY}&autoload=false&libraries=services,clusterer,drawing`;
 
   mapScript.onload = () => {
