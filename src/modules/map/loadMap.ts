fc179d6e30cf2852e6a5e86073e261c2e8e9bc4b
최유진 <yjyj0774@gmail.com>
2024-10-02 12:37:27 +0900

    :package: 최유진 : initMap 모듈 불러오기

--- src/modules/map/loadMap.ts
+++ src/modules/map/loadMap.ts
@@ -1,27 +1,15 @@
+import { initMap } from "./initMap";
+
 export const loadMap = () => {
-  return new Promise((resolve, reject) => {
-    if (window.kakao && window.kakao.maps) {
-      resolve(window.kakao.maps);
-      return;
-    }
+  const mapScript = document.createElement("script");
 
-    const mapScript = document.createElement("script");
-    mapScript.src = `//dapi.kakao.com/v2/maps/sdk.js?appkey=${process.env.VUE_APP_API_KEY}&autoload=false&libraries=services,clusterer,drawing`;
+  mapScript.src = `//dapi.kakao.com/v2/maps/sdk.js?appkey=${process.env.VUE_APP_API_KEY}&autoload=false&libraries=services,clusterer,drawing`;
 
-    mapScript.onload = () => {
-      if (window.kakao && window.kakao.maps) {
-        window.kakao.maps.load(() => {
-          resolve(window.kakao.maps);
-        });
-      } else {
-        reject(new Error("Kakao Maps API failed to load correctly"));
-      }
-    };
+  mapScript.onload = () => {
+    window.kakao.maps.load(() => {
+      initMap();
+    });
+  };
 
-    mapScript.onerror = () => {
-      reject(new Error("Failed to load Kakao Maps API"));
-    };
-
-    document.head.appendChild(mapScript);
-  });
+  document.head.appendChild(mapScript);
 };
