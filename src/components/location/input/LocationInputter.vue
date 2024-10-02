404ec7205b2495562096c99b46ff7b7b0ab4bfe3
최유진 <yjyj0774@gmail.com>
2024-10-02 11:40:52 +0900

    :hammer: 최유진 : input value를 searchValue에 저장

--- src/components/location/input/LocationInputter.vue
+++ src/components/location/input/LocationInputter.vue
@@ -3,6 +3,7 @@
     <input
       :type="locationStatic.inputType"
       :placeholder="locationStatic.inputPlaceholder"
+      v-model="searchValue"
       class="w-[500px] h-10 rounded-2xl border-2 px-4 my-6 hover:border-main-color focus:outline-none focus:border-main-color"
     />
   </div>
@@ -13,13 +14,20 @@
  * @yuxincxoi 24.10.01
  * * 장소, 주소 검색창
  */
-import { defineComponent } from "vue";
+import { defineComponent, ref } from "vue";
 import { locationStatic } from "@/static/components/location/location.static";
 
 export default defineComponent({
   data() {
     return {
       locationStatic,
+    };
+  },
+  setup() {
+    const searchValue = ref("");
+
+    return {
+      searchValue,
     };
   },
 });
